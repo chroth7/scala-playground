8f5b501d4d85ed276fc0edae41278942be2ab449
Christian Roth <chroth@me.com>
2020-01-13 14:15:14 +0100

    Add some of chapter 5.

--- src/main/scala/example/underscore5/Underscore5.scala
+++ src/main/scala/example/underscore5/Underscore5.scala
@@ -1,9 +1,59 @@
 package example.underscore5
 
-sealed trait Color {
-  def r: Int
-  def g: Int
-  def b: Int
-  def isLight(): Boolean = (r + g + b) / 2 < 380
+sealed trait Result[A]
+final case class Success[A](s: A) extends Result[A]
+final case class Failure[A](f: String) extends Result[A]
+
+sealed trait LinkedList[A] {
+  def apply(n: Int): Result[A] = {
+    // if (n < 0) throw new Exception("Subzero index")
+    if (n < 0) Failure("Subzero Index") else
+      this match {
+        // case End() => throw new Exception("Index out of bounds")
+        case End() => Failure("Index out of bounds")
+        case Pair(h, t) => {
+          // if (n == 0) h else t(n - 1)
+          if (n == 0) Success(h) else t(n - 1)
+        }
+      }
+  }
+
+  def fold[B](end: B, f: (A, B) => B): B = {
+    this match {
+      case End() =>  end
+      case Pair(hd, tl) => f(hd, tl.fold(end, f))
+    }
+  }
+
+  def length: Int = {
+    def f[A] = (_: A, acc: Int) => acc + 1
+    this.fold(0, f)
+  }
+
+  // short:
+  // def length: Int = fold(0, (_, a) => a + 1)
+
+  def lengthWithoutFold: Int = {
+    this match {
+      case End() => 0
+      case Pair(h, t) => 1 + t.lengthWithoutFold
+    }
+  }
+  def contains(el: A): Boolean = {
+    this match {
+      case End() => false
+      case Pair(h, t) => if (h == el) true else t.contains(el)
+    }
+  }
 }
+
+final case class End[A]() extends LinkedList[A]
+final case class Pair[A](head: A, tail: LinkedList[A]) extends LinkedList[A]
+
+// final case class IntList() extends LinkedList[Int] {
+//   def sum: Int = {
+//     def s = (v: Int, a: Int) => v + a
+//     this.fold(0, s)
+//   }
+// }
 
--- src/test/scala/underscore5/U5Spec.scala
+++ src/test/scala/underscore5/U5Spec.scala
@@ -2,8 +2,35 @@ import org.scalatest._
 import example.underscore5._
 
 class U5Spec extends FunSuite with DiagrammedAssertions {
-  test("Can draw a circle") {
-    assert("Hello".startsWith("H"))
+  test("LinkedList") {
+    val ll1 = Pair(1, Pair(5, End()))
+    val ll2 = Pair(1, Pair(5, Pair(10, End())))
+    assert(ll1.length.equals(2))
+    assert(ll2.length.equals(3))
 
+    assert(ll1.contains(1).equals(true))
+    assert(ll1.contains(2).equals(false))
+    // does not compile:
+    // assert(ll1.contains("2").equals(false))
+    
+    assert(ll1(0) == Success(1))
+    assert(ll1(1) == Success(5))
+    assert(ll1(123) == Failure("Index out of bounds"))
+    assert(ll1(-123) == Failure("Subzero Index"))
+    // assert(try {
+    //   ll1(2) 
+    //   false
+    // } catch {
+    //   case e: Exception => true
+    // })
+
+    // SUM INLINE
+    assert(ll1.fold(0, (v, a: Int) => v + a) == 6)
+    assert(ll2.fold(0, (v, a: Int) => v + a) == 16)
+  }
+
+  test("IntList") {
+    // val il1: IntList = Pair(1, Pair(5, End()))
+    // assert(il1.sum() == 6)
   }
 }
